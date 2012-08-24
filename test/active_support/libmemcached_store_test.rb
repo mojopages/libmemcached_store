eb84c8dd84833ae35d379de2d3ac1df65b779917
ccocchi <cocchi.c@gmail.com>
2012-08-24 13:13:14 +0200

    Update test to reflect removal of AS::Entry
    * Key expiration is already managed by memcached so don't need to test that.
    * Compression still needs to work
    * Nil storage is dropped

--- test/active_support/libmemcached_store_test.rb
+++ test/active_support/libmemcached_store_test.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'test_helper'
 require 'memcached'
 require 'active_support'
@@ -7,67 +9,101 @@ require 'active_support/cache/libmemcached_store'
 
 # Make it easier to get at the underlying cache options during testing.
 class ActiveSupport::Cache::LibmemcachedStore
-  delegate :options, :to => '@cache'
+  def client_options
+    @cache.options
+  end
+end
+
+class MockUser
+  def cache_key
+    'foo'
+  end
 end
 
 module CacheStoreBehavior
-  def test_should_read_and_write_strings
-    assert_equal true, @cache.write('foo', 'bar')
-    assert_equal 'bar', @cache.read('foo')
-  end
-
-  def test_should_overwrite
-    @cache.write('foo', 'bar')
-    @cache.write('foo', 'baz')
-    assert_equal 'baz', @cache.read('foo')
-  end
-
   def test_fetch_without_cache_miss
     @cache.write('foo', 'bar')
-    @cache.expects(:write).never
+    @cache.expects(:write_entry).never
     assert_equal 'bar', @cache.fetch('foo') { 'baz' }
   end
 
   def test_fetch_with_cache_miss
-    @cache.expects(:write).with('foo', 'baz', @cache.options)
+    @cache.expects(:write_entry).with('foo', 'baz', nil)
     assert_equal 'baz', @cache.fetch('foo') { 'baz' }
   end
 
   def test_fetch_with_forced_cache_miss
     @cache.write('foo', 'bar')
-    @cache.expects(:read).never
-    @cache.expects(:write).with('foo', 'bar', @cache.options.merge(:force => true))
-    @cache.fetch('foo', :force => true) { 'bar' }
+    @cache.expects(:read_entry).never
+    @cache.expects(:write_entry).with('foo', 'baz', force: true)
+    assert_equal 'baz', @cache.fetch('foo', force: true) { 'baz' }
   end
 
-  def test_fetch_with_cached_nil
-    @cache.write('foo', nil)
-    @cache.expects(:write).never
-    assert_nil @cache.fetch('foo') { 'baz' }
+  def test_fetch_with_cached_false
+    @cache.write('foo', false)
+    refute @cache.fetch('foo') { raise }
+  end
+
+  def test_fetch_with_raw_object
+    o = Object.new
+    o.instance_variable_set :@foo, 'bar'
+    assert_equal o, @cache.fetch('foo', raw: true) { o }
+  end
+
+  def test_fetch_with_cache_key
+    u = MockUser.new
+    @cache.write(u.cache_key, 'bar')
+    assert_equal 'bar', @cache.fetch(u) { raise }
+  end
+
+  def test_should_read_and_write_strings
+    assert @cache.write('foo', 'bar')
+    assert_equal 'bar', @cache.read('foo')
   end
 
   def test_should_read_and_write_hash
-    assert_equal true, @cache.write('foo', {:a => "b"})
-    assert_equal({:a => "b"}, @cache.read('foo'))
+    assert @cache.write('foo', { a: 'b' })
+    assert_equal({ a: 'b' }, @cache.read('foo'))
   end
 
   def test_should_read_and_write_integer
-    assert_equal true, @cache.write('foo', 1)
+    assert @cache.write('foo', 1)
     assert_equal 1, @cache.read('foo')
   end
 
   def test_should_read_and_write_nil
-    assert_equal true, @cache.write('foo', nil)
+    assert @cache.write('foo', nil)
     assert_equal nil, @cache.read('foo')
   end
 
   def test_should_read_and_write_false
     assert @cache.write('foo', false)
-    if ActiveSupport::VERSION::MAJOR == 3 && ActiveSupport::VERSION::MINOR == 0
-      assert_equal nil, @cache.read('foo')
-    else
-      assert_equal false, @cache.read('foo')
-    end
+    assert_equal false, @cache.read('foo')
+  end
+
+  def test_read_and_write_compressed_data
+    @cache.write('foo', 'bar', :compress => true, :compress_threshold => 1)
+    assert_equal 'bar', @cache.read('foo')
+  end
+
+  def test_write_should_overwrite
+    @cache.write('foo', 'bar')
+    @cache.write('foo', 'baz')
+    assert_equal 'baz', @cache.read('foo')
+  end
+
+  def test_write_compressed_data
+    @cache.write('foo', 'bar', :compress => true, :compress_threshold => 1, :raw => true)
+    assert_equal Zlib::Deflate.deflate('bar'), @cache.instance_variable_get(:@cache).get('foo', false)
+  end
+
+  def test_read_miss
+    assert_nil @cache.read('foo')
+  end
+
+  def test_read_should_return_a_different_object_id_each_time_it_is_called
+    @cache.write('foo', 'bar')
+    refute_equal @cache.read('foo').object_id, @cache.read('foo').object_id
   end
 
   def test_read_multi
@@ -77,94 +113,75 @@ module CacheStoreBehavior
     assert_equal({"foo" => "bar", "fu" => "baz"}, @cache.read_multi('foo', 'fu'))
   end
 
-  def test_read_multi_with_expires
-    @cache.write('foo', 'bar', :expires_in => 0.001)
+  def test_read_multi_with_array
+    @cache.write('foo', 'bar')
     @cache.write('fu', 'baz')
-    @cache.write('fud', 'biz')
-    sleep(0.002)
-    assert_equal({"fu" => "baz"}, @cache.read_multi('foo', 'fu'))
+    assert_equal({"foo" => "bar", "fu" => "baz"}, @cache.read_multi(['foo', 'fu']))
   end
 
-  def test_read_and_write_compressed_small_data
-    @cache.write('foo', 'bar', :compress => true)
-    raw_value = @cache.send(:read_entry, 'foo', {}).raw_value
-    assert_equal 'bar', @cache.read('foo')
-    value = Marshal.load(raw_value) rescue raw_value
-    assert_equal 'bar', value
+  def test_read_multi_with_raw
+    @cache.write('foo', 'bar', :raw => true)
+    @cache.write('fu', 'baz', :raw => true)
+    assert_equal({"foo" => "bar", "fu" => "baz"}, @cache.read_multi('foo', 'fu'))
   end
 
-  def test_read_and_write_compressed_large_data
-    @cache.write('foo', 'bar', :compress => true, :compress_threshold => 2)
-    raw_value = @cache.send(:read_entry, 'foo', {}).raw_value
-    assert_equal 'bar', @cache.read('foo')
-    assert_equal 'bar', Marshal.load(Zlib::Inflate.inflate(raw_value))
-  end
-
-  def test_read_and_write_compressed_nil
-    @cache.write('foo', nil, :compress => true)
-    assert_nil @cache.read('foo')
+  def test_read_multi_with_compress
+    @cache.write('foo', 'bar', :compress => true, :compress_threshold => 1)
+    @cache.write('fu', 'baz', :compress => true, :compress_threshold => 1)
+    assert_equal({"foo" => "bar", "fu" => "baz"}, @cache.read_multi('foo', 'fu'))
   end
 
   def test_cache_key
-    obj = Object.new
-    def obj.cache_key
-      :foo
-    end
-    @cache.write(obj, "bar")
-    assert_equal "bar", @cache.read("foo")
+    o = MockUser.new
+    @cache.write(o, 'bar')
+    assert_equal 'bar', @cache.read('foo')
   end
 
   def test_param_as_cache_key
     obj = Object.new
     def obj.to_param
-      "foo"
+      'foo'
     end
-    @cache.write(obj, "bar")
-    assert_equal "bar", @cache.read("foo")
+    @cache.write(obj, 'bar')
+    assert_equal 'bar', @cache.read('foo')
   end
 
   def test_array_as_cache_key
-    @cache.write([:fu, "foo"], "bar")
-    assert_equal "bar", @cache.read("fu/foo")
+    @cache.write([:fu, 'foo'], 'bar')
+    assert_equal 'bar', @cache.read('fu/foo')
   end
 
   def test_hash_as_cache_key
-    @cache.write({:foo => 1, :fu => 2}, "bar")
-    assert_equal "bar", @cache.read("foo=1/fu=2")
+    @cache.write({:foo => 1, :fu => 2}, 'bar')
+    assert_equal 'bar', @cache.read('foo=1/fu=2')
   end
 
   def test_keys_are_case_sensitive
-    @cache.write("foo", "bar")
-    assert_nil @cache.read("FOO")
+    @cache.write('foo', 'bar')
+    assert_nil @cache.read('FOO')
+  end
+
+  def test_keys_with_spaces
+    assert_equal 'baz', @cache.fetch('foo bar') { 'baz' }
   end
 
   def test_exist
     @cache.write('foo', 'bar')
-    assert_equal true, @cache.exist?('foo')
-    assert_equal false, @cache.exist?('bar')
-  end
-
-  def test_nil_exist
-    @cache.write('foo', nil)
-    assert_equal true, @cache.exist?('foo')
+    assert @cache.exist?('foo')
+    refute @cache.exist?('bar')
   end
 
   def test_delete
     @cache.write('foo', 'bar')
     assert @cache.exist?('foo')
-    assert_equal true, @cache.delete('foo')
-    assert !@cache.exist?('foo')
+    assert @cache.delete('foo')
+    refute @cache.exist?('foo')
   end
 
   def test_delete_with_unexistent_key
     @cache.expects(:log_error).never
-    assert !@cache.exist?('foo')
-    assert_equal false, @cache.delete('foo')
-  end
-
-  def test_read_should_return_a_different_object_id_each_time_it_is_called
-    @cache.write('foo', 'bar')
-    refute_equal @cache.read('foo').object_id, @cache.read('foo').object_id
+    refute @cache.exist?('foo')
+    refute @cache.delete('foo')
   end
 
   def test_store_objects_should_be_immutable
@@ -179,112 +196,41 @@ module CacheStoreBehavior
     assert_equal 'baz', bar.gsub!(/r/, 'z')
   end
 
-  def test_expires_in
-    time = Time.local(2008, 4, 24)
-    Time.stubs(:now).returns(time)
-
-    @cache.write('foo', 'bar', :expires_in => 45)
-    assert_equal 'bar', @cache.read('foo')
-
-    Time.stubs(:now).returns(time + 30)
-    assert_equal 'bar', @cache.read('foo')
-
-    Time.stubs(:now).returns(time + 61)
-    assert_nil @cache.read('foo')
-  end
-
-  def test_expires_in_as_activesupport_duration
-    time = Time.local(2012, 02, 03)
-    Time.stubs(:now).returns(time)
-
-    @cache.write('foo', 'bar', :expires_in => 1.minute)
-    assert_equal 'bar', @cache.read('foo')
-
-    Time.stubs(:now).returns(time + 30)
-    assert_equal 'bar', @cache.read('foo')
-
-    Time.stubs(:now).returns(time + 61)
-    assert_nil @cache.read('foo')
-  end
-
-  def test_expires_in_as_float
-    time = Time.local(2012, 02, 03)
-    Time.stubs(:now).returns(time)
-
-    @cache.write('foo', 'bar', :expires_in => 60.0)
-    assert_equal 'bar', @cache.read('foo')
-
-    Time.stubs(:now).returns(time + 30)
-    assert_equal 'bar', @cache.read('foo')
-
-    Time.stubs(:now).returns(time + 61)
-    assert_nil @cache.read('foo')
-  end
-
-  def test_race_condition_protection
-    time = Time.now
-    @cache.write('foo', 'bar', :expires_in => 60)
-    Time.stubs(:now).returns(time + 61)
-    result = @cache.fetch('foo', :race_condition_ttl => 10) do
-      assert_equal 'bar', @cache.read('foo')
-      "baz"
-    end
-    assert_equal "baz", result
-  end
-
-  def test_race_condition_protection_is_limited
-    time = Time.now
-    @cache.write('foo', 'bar', :expires_in => 60)
-    Time.stubs(:now).returns(time + 71)
-    result = @cache.fetch('foo', :race_condition_ttl => 10) do
-      assert_equal nil, @cache.read('foo')
-      "baz"
-    end
-    assert_equal "baz", result
-  end
-
-  def test_race_condition_protection_is_safe
-    time = Time.now
-    @cache.write('foo', 'bar', :expires_in => 60)
-    Time.stubs(:now).returns(time + 61)
-    begin
-      @cache.fetch('foo', :race_condition_ttl => 10) do
-        assert_equal 'bar', @cache.read('foo')
-        raise ArgumentError.new
-      end
-    rescue ArgumentError
-    end
-    assert_equal "bar", @cache.read('foo')
-    Time.stubs(:now).returns(time + 71)
-    assert_nil @cache.read('foo')
-  end
-
   def test_crazy_key_characters
     crazy_key = "#/:*(<+=> )&$%@?;'\"\'`~-"
-    assert_equal true, @cache.write(crazy_key, "1", :raw => true)
+    assert @cache.write(crazy_key, "1", :raw => true)
     assert_equal "1", @cache.read(crazy_key)
     assert_equal "1", @cache.fetch(crazy_key)
-    assert_equal true, @cache.delete(crazy_key)
+    assert @cache.delete(crazy_key)
+    refute @cache.exist?(crazy_key)
     assert_equal "2", @cache.fetch(crazy_key, :raw => true) { "2" }
     assert_equal 3, @cache.increment(crazy_key)
     assert_equal 2, @cache.decrement(crazy_key)
   end
 
-  # def test_really_long_keys
-  #   key = ""
-  #   900.times{key << "x"}
-  #   assert @cache.write(key, "bar")
-  #   assert_equal "bar", @cache.read(key)
-  #   assert_equal "bar", @cache.fetch(key)
-  #   assert_nil @cache.read("#{key}x")
-  #   assert_equal({key => "bar"}, @cache.read_multi(key))
-  #   assert @cache.delete(key)
-  # end
+  def test_really_long_keys
+    key = "a" * 251
+    assert @cache.write(key, "bar")
+    assert_equal "bar", @cache.read(key)
+    assert_equal "bar", @cache.fetch(key)
+    assert_nil @cache.read("#{key}x")
+    assert_equal({key => "bar"}, @cache.read_multi(key))
+    assert @cache.delete(key)
+    refute @cache.exist?(key)
+    assert @cache.write(key, '2', :raw => true)
+    assert_equal 3, @cache.increment(key)
+    assert_equal 2, @cache.decrement(key)
+  end
+
+  def test_really_long_keys_with_namespace
+    @cache = ActiveSupport::Cache.lookup_store(:libmemcached_store, :expires_in => 60, :namespace => 'namespace')
+    test_really_long_keys
+  end
 end
 
 module CacheIncrementDecrementBehavior
   def test_increment
-    @cache.write('foo', 1, :raw => true)
+    @cache.write('foo', '1', :raw => true)
     assert_equal 1, @cache.read('foo').to_i
     assert_equal 2, @cache.increment('foo')
     assert_equal 2, @cache.read('foo').to_i
@@ -293,7 +239,7 @@ module CacheIncrementDecrementBehavior
   end
 
   def test_decrement
-    @cache.write('foo', 3, :raw => true)
+    @cache.write('foo', '3', :raw => true)
     assert_equal 3, @cache.read('foo').to_i
     assert_equal 2, @cache.decrement('foo')
     assert_equal 2, @cache.read('foo').to_i
@@ -308,12 +254,29 @@ module CacheIncrementDecrementBehavior
   end
 end
 
+module CacheCompressBehavior
+  def test_read_and_write_compressed_small_data
+    @cache.write('foo', 'bar', :compress => true)
+    raw_value = @cache.send(:read_entry, 'foo', {}).raw_value
+    assert_equal 'bar', @cache.read('foo')
+    value = Marshal.load(raw_value) rescue raw_value
+    assert_equal 'bar', value
+  end
+
+  def test_read_and_write_compressed_large_data
+    @cache.write('foo', 'bar', :compress => true, :compress_threshold => 2)
+    raw_value = @cache.send(:read_entry, 'foo', {}).raw_value
+    assert_equal 'bar', @cache.read('foo')
+    assert_equal 'bar', Marshal.load(Zlib::Inflate.inflate(raw_value))
+  end
+end
+
 class LibmemcachedStoreTest < MiniTest::Unit::TestCase
   include CacheStoreBehavior
   include CacheIncrementDecrementBehavior
 
   def setup
-    @cache = ActiveSupport::Cache.lookup_store(:libmemcached_store, :expires_in => 60)
+    @cache = ActiveSupport::Cache.lookup_store(:libmemcached_store, expires_in: 60)
     @cache.clear
     @cache.silence!
   end
@@ -332,26 +295,34 @@ class LibmemcachedStoreTest < MiniTest::Unit::TestCase
   end
 
   def test_should_enable_consistent_ketema_hashing_by_default
-    assert_equal :consistent_ketama, @cache.options[:distribution]
+    assert_equal :consistent_ketama, @cache.client_options[:distribution]
   end
 
   def test_should_not_enable_non_blocking_io_by_default
-    assert_equal false, @cache.options[:no_block]
+    assert_equal false, @cache.client_options[:no_block]
   end
 
   def test_should_not_enable_server_failover_by_default
-    assert_nil @cache.options[:failover]
+    assert_nil @cache.client_options[:failover]
   end
 
   def test_should_allow_configuration_of_custom_options
-    options = {
-      :tcp_nodelay => true,
-      :distribution => :modula
-    }
+    options = { client: { tcp_nodelay: true, distribution: :modula } }
 
     store = ActiveSupport::Cache.lookup_store :libmemcached_store, 'localhost', options
 
-    assert_equal :modula, store.options[:distribution]
-    assert_equal true, store.options[:tcp_nodelay]
+    assert_equal :modula, store.client_options[:distribution]
+    assert_equal true, store.client_options[:tcp_nodelay]
+  end
+
+  def test_should_allow_mute_and_silence
+    cache = ActiveSupport::Cache.lookup_store :libmemcached_store, 'localhost'
+    cache.mute do
+      assert cache.write('foo', 'bar')
+      assert_equal 'bar', cache.read('foo')
+    end
+    refute cache.silence?
+    cache.silence!
+    assert cache.silence?
   end
 end
